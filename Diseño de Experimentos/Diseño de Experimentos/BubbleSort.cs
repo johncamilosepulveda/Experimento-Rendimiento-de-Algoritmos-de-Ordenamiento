483f51c83405f648fa00fb6c50c6d015c23f1913
Jorge Eliecer Lievano Solis <jorge.lievano@correo.icesi.edu.co>
2019-02-09 16:41:35 -0500

    Agrego clase con el metodo BubbleSort

---
+++ Diseño de Experimentos/Diseño de Experimentos/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diseño_de_Experimentos
+{
+    class BubbleSort
+    {
+
+
+        public int[] OrdenarBubbleSort(int[] arreglo)
+        {
+            int n = arreglo.Length;
+            for (int i=2; i < n; i++)
+            {
+                for(int j=0; j < (n - i); j++)
+                {
+                    if (arreglo[j] > arreglo[j + 1])
+                    {
+                        int aux = arreglo[j];
+                        arreglo[j] = arreglo[j + 1];
+                        arreglo[j + 1] = aux;
+                    }
+                }
+            }
+            return arreglo;
+        }
+
+
+
+
+    }
+}
