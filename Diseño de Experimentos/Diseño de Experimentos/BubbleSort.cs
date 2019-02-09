5fdf68e0fa6116a2c3923ab508f105072e75f81a
Jorge Eliecer Lievano Solis <jorge.lievano@correo.icesi.edu.co>
2019-02-09 18:50:23 -0500

    Corrección a BubbleSort para que recorra todo el arreglo. Agrego InsertionSort

--- Diseño de Experimentos/Diseño de Experimentos/BubbleSort.cs
+++ Diseño de Experimentos/Diseño de Experimentos/BubbleSort.cs
@@ -7,13 +7,12 @@ namespace Diseño_de_Experimentos
     class BubbleSort
     {
 
-
         public int[] OrdenarBubbleSort(int[] arreglo)
         {
             int n = arreglo.Length;
             for (int i=2; i < n; i++)
             {
-                for(int j=0; j < (n - i); j++)
+                for(int j=0; j <= (n - i); j++)
                 {
                     if (arreglo[j] > arreglo[j + 1])
                     {
---
+++ Diseño de Experimentos/Diseño de Experimentos/InsertionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diseño_de_Experimentos
+{
+    class InsertionSort
+    {
+
+        public  int[] OrdenarInsertionSort(int[] arreglo)
+        {
+            int i, j,n, aux;
+            n = arreglo.Length;
+            for(i=1; i < n; i++)
+            {
+                aux = arreglo[i];
+                j = i - 1;
+                while (j >= 0 && arreglo[j] > aux)
+                {
+                    arreglo[j + 1] = arreglo[j];
+                    j--;
+                }
+                arreglo[j + 1] = aux;
+            }
+            return arreglo;
+        }
+        
+
+    }
+}
