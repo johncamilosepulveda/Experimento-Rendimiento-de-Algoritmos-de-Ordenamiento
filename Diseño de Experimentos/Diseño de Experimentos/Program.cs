2b53b445d28d4846fce524898e409b325ddac8f8
John Camilo Sepulveda Serna <john.sepulveda@correo.icesi.edu.co>
2019-02-09 13:46:31 -0500

    Creo el programa por consola inicial para comenzar a programar los algoritmos de ordenamiento.

---
+++ Diseño de Experimentos/Diseño de Experimentos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diseño_de_Experimentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
